655dfa0407a8312fd37f626be8016bcb6e94aa21
Matthew Hall <matthew@quickbeam.me.uk>
2021-03-07 18:42:27 +0000

    Add support for parsing data records

--- src/main.zig
+++ src/main.zig
@@ -8,11 +8,18 @@ const ParseError = error{
     Eof,
     NotNormalHeader,
     InvalidReserveBit,
+    UnrecognisedLocalType,
 };
 
 const MsgType = enum {
     definition,
     data,
+};
+
+const DevField = struct {
+    number: u8,
+    size: u8,
+    developer_data_index: u8,
 };
 
 const Field = struct {
@@ -26,11 +33,13 @@ const Definition = struct {
     local_msg_type: u4,
     global_msg_type: u16,
     fields: std.ArrayList(Field),
+    dev_fields: std.ArrayList(DevField),
 
     const Self = @This();
 
     fn deinit(self: *Self) void {
         self.fields.deinit();
+        self.dev_fields.deinit();
     }
 };
 
@@ -40,9 +49,21 @@ const Header = struct {
     data_size: u32,
 };
 
+const Data = struct {
+    local_msg_type: u4,
+    data: std.ArrayList(u8),
+
+    const Self = @This();
+
+    fn deinit(self: *Self) void {
+        self.data.deinit();
+    }
+};
+
 const Event = union(enum) {
     header: Header,
     definition: Definition,
+    data: Data,
     // TODO data record
 };
 
@@ -50,15 +71,31 @@ fn Parser(comptime Reader: anytype) type {
     return struct {
         reader: Reader,
         allocator: *std.mem.Allocator,
+        definitions: []?Definition,
         had_header: bool = false,
 
         const Self = @This();
 
-        fn init(allocator: *std.mem.Allocator, reader: Reader) Self {
-            return .{
+        fn init(allocator: *std.mem.Allocator, reader: Reader) !Self {
+            var self = Self{
                 .allocator = allocator,
                 .reader = reader,
+                .definitions = try allocator.alloc(?Definition, std.math.maxInt(u4)),
             };
+            var i: usize = 0;
+            while (i < self.definitions.len) : (i += 1) {
+                self.definitions[i] = null;
+            }
+            return self;
+        }
+
+        fn deinit(self: *Self) void {
+            for (self.definitions) |*def| {
+                if (def.*) |*d| {
+                    d.deinit();
+                }
+            }
+            self.allocator.destroy(self.definitions.ptr);
         }
 
         fn parseHeader(self: *Self) !Event {
@@ -107,15 +144,20 @@ fn Parser(comptime Reader: anytype) type {
             }
             def.fields = fields;
 
+            var dev_fields = std.ArrayList(DevField).init(self.allocator);
             if (dev) {
-                const dev_fields = try self.reader.readInt(u8, def.endian);
-                std.log.debug("skipping {} dev fields", .{dev_fields});
+                const num_dev_fields = try self.reader.readInt(u8, def.endian);
+                try dev_fields.ensureCapacity(num_dev_fields);
                 i = 0;
-                while (i < dev_fields) : (i += 1) {
-                    // TODO store dev fields?
-                    _ = try self.reader.readInt(u24, def.endian);
+                while (i < num_dev_fields) : (i += 1) {
+                    var field: DevField = undefined;
+                    field.number = try self.reader.readInt(u8, def.endian);
+                    field.size = try self.reader.readInt(u8, def.endian);
+                    field.developer_data_index = try self.reader.readInt(u8, def.endian);
+                    dev_fields.appendAssumeCapacity(field);
                 }
             }
+            def.dev_fields = dev_fields;
 
             return def;
         }
@@ -125,17 +167,36 @@ fn Parser(comptime Reader: anytype) type {
             const header_type = header & (1 << 7);
             if (header_type != 0) return ParseError.NotNormalHeader; // TODO: handle timestamp headers
             const typ: MsgType = if (header & (1 << 6) != 0) .definition else .data;
-            const dev = if (header & (1 << 5) != 0) true else false;
+            const reserved = header & (1 << 4);
+            if (reserved != 0) return ParseError.InvalidReserveBit;
+            const local_typ = header & (0b00000111);
 
             if (typ == .definition) {
-                const reserved = header & (1 << 4);
-                if (reserved != 0) return ParseError.InvalidReserveBit;
-                const local_typ = header & (0b00000111);
+                const dev = if (header & (1 << 5) != 0) true else false;
                 var def = try self.parseRecordDefinition(dev);
                 def.local_msg_type = @intCast(u4, local_typ);
+                if (self.definitions[def.local_msg_type]) |*old_def| {
+                    old_def.deinit();
+                }
+                self.definitions[def.local_msg_type] = def;
                 return Event{ .definition = def };
             } else {
-                @panic("todo, handle data"); // TODO
+                const def = &self.definitions[local_typ];
+                _ = def.* orelse return ParseError.UnrecognisedLocalType;
+                var bytes: usize = 0;
+                for (def.*.?.fields.items) |field| {
+                    bytes += field.size;
+                }
+                var data = Data{
+                    .local_msg_type = @intCast(u4, local_typ),
+                    .data = std.ArrayList(u8).init(self.allocator),
+                };
+                try data.data.resize(bytes);
+                const read = try self.reader.read(data.data.items);
+                if (read != bytes) {
+                    return ParseError.Eof;
+                }
+                return Event{ .data = data };
             }
         }
 
@@ -158,14 +219,15 @@ pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
 
-    var parser = Parser(@TypeOf(reader)).init(&gpa.allocator, reader);
+    var parser = try Parser(@TypeOf(reader)).init(&gpa.allocator, reader);
+    defer parser.deinit();
+
     while (try parser.next()) |*ev| {
         switch (ev.*) {
             .header => |hdr| {
                 std.log.debug("got file header: protocol version {}, profile version {}, size {}", .{ hdr.protocol_version, hdr.profile_version, hdr.data_size });
             },
             .definition => |*def| {
-                defer def.deinit();
                 std.log.debug("def of type {s}", .{defs.MsgTypes.toString(def.global_msg_type)});
                 for (def.fields.items) |field| {
                     if (def.global_msg_type == defs.MsgTypes.file_id) {
@@ -174,6 +236,21 @@ pub fn main() !void {
                         std.log.debug("  got unknown field", .{});
                     }
                 }
+            },
+            .data => |*data| blk: {
+                defer data.deinit();
+                const def = parser.definitions[data.local_msg_type] orelse unreachable;
+                if (def.global_msg_type != defs.MsgTypes.file_id) {
+                    std.log.debug("data for local type {}", .{data.local_msg_type});
+                    break :blk;
+                }
+                var file_id_reader = std.io.fixedBufferStream(data.data.items).reader();
+                const file_created = try file_id_reader.readInt(u32, def.endian);
+                const typ = try file_id_reader.readInt(u8, def.endian);
+                const manufacturer = try file_id_reader.readInt(u16, def.endian);
+                const product = try file_id_reader.readInt(u16, def.endian);
+                const serial_number = try file_id_reader.readInt(u32, def.endian);
+                std.log.debug("data for file id: file_created {}, typ: {}, manufacturer: {}, product: {}, serial number: {}", .{ file_created, typ, manufacturer, product, serial_number });
             },
         }
     }
